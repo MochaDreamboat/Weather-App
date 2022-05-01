7adb9a97a04ef90f74edfaa5a56bb9e8928e2547
MochaDreamboat <jersotmac@gmail.com>
2022-04-30 19:13:06 -0500

    Added basic fetch script for acquiring weather data via JSON response.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
-console.log("Hi");
+
+function getWeatherResponse () {
+
+    let weatherData;
+    weatherInfo = fetch('http://api.openweathermap.org/data/2.5/weather?q=Addison&APPID=7498ebf6859f10448d9e3958cea02c93');
+    
+    weatherInfo
+        .then(function (response) {
+        return response.json();
+        })
+        .then( (weatherData) => {
+            console.log(weatherData);
+            console.log(weatherData.main.temp);
+        })
+        .catch(err => console.log(err));
+
+}
+
+getWeatherResponse()
