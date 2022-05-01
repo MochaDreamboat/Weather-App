69038c2cf9e678f27886061272c2d0754bc9c6fe
mochadreamboat <jersotmac@gmail.com>
2022-05-01 18:41:29 -0500

    Added access to the following: temperature, humidity, pressure, general description and logging values in console.

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
-
+import fetch from "node-fetch";
 function getWeatherResponse () {
 
     let weatherData;
-    weatherInfo = fetch('http://api.openweathermap.org/data/2.5/weather?q=Addison&APPID=7498ebf6859f10448d9e3958cea02c93');
+    let weatherInfo = fetch('http://api.openweathermap.org/data/2.5/weather?q=Addison&APPID=7498ebf6859f10448d9e3958cea02c93');
     
     weatherInfo
         .then(function (response) {
         return response.json();
         })
-        .then( (weatherData) => {
-            console.log(weatherData);
-            console.log(weatherData.main.temp);
+        .then( (response) => {
+            let weatherData = response;
+            console.log('City:', weatherData.name);
+            console.log('Conditions:', weatherData.weather[0].description);
+            console.log(weatherData.main.temp, 'Kelvin');
+            console.log(weatherData.main.pressure);
+            console.log(weatherData.main.humidity, '%');
         })
         .catch(err => console.log(err));
 
