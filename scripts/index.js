05ea6f0c073c99dfe05f49d572b2e45d6ef39604
mochadreamboat <jersotmac@gmail.com>
2022-05-05 18:40:27 -0500

    Refactored weather data object into separate constructor. Linted via Beautify.

--- scripts/index.js
+++ scripts/index.js
@@ -5,28 +5,31 @@ const searchButton = document.getElementById('search');
 // from API and returns data in an object.
 
 // To be returned UI generating function displaying data to user.
-function getWeatherResponse (searchedCity) {
 
+
+function WeatherInfo(data) {
+    this.city = data.name;
+    this.conditions = data.weather[0].description;
+    this.temperature = data.main.temp;
+    this.pressure = data.main.pressure;
+    this.humidity = data.main.humidity;
+}
+
+function getWeatherResponse(searchedCity) {
     let weatherInfo = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&APPID=7498ebf6859f10448d9e3958cea02c93`);
-    
+
     weatherInfo
         .then(response => {
             if (response.status == 200) {
-            return response.json();
-        } else {
-            alert('Please enter a valid city');
-            throw new Error;
+                return response.json();
+            } else {
+                alert('Please enter a valid city');
+                throw new Error;
             }
         })
-        .then( (response) => {
-            let userWeatherData = {};
-            let weatherData = response;
-            userWeatherData.city = weatherData.name;
-            userWeatherData.conditions = weatherData.weather[0].description;
-            userWeatherData.temperature = weatherData.main.temp;
-            userWeatherData.pressure = weatherData.main.pressure;
-            userWeatherData.humidity = weatherData.main.humidity;
-            console.log(userWeatherData);   
+        .then((response) => {
+            let userWeatherData = new WeatherInfo(response);
+            console.log(userWeatherData);
             return userWeatherData;
         })
         .catch(err => console.log(err));
