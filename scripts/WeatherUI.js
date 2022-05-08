a825f251084e3a595d0bcc70d1d00391057269e8
mochadreamboat <jersotmac@gmail.com>
2022-05-08 14:30:35 -0500

    Added icon switching dependent on weather conditions.

--- scripts/WeatherUI.js
+++ scripts/WeatherUI.js
@@ -3,7 +3,6 @@
 export { displayWeatherData };
 
 function displayWeatherData (data) {
-
     // Targets weather elements in DOM...
     let city = document.getElementById('city');
     // Find a way to associate conditions with respective icon...
@@ -14,9 +13,33 @@ function displayWeatherData (data) {
     // Display more data (i.e. UV Index)
 
     // Update with respective data from Weather object.
-
+    conditionsIcon.setAttribute('src', setIcon(data.conditions));
     city.textContent = `${data.city}`;
     temperature.textContent = `${data.temperature} °F`;
     conditions.textContent = `Conditions: ${data.conditions}`;
     humidity.textContent = `Humidity: ${data.humidity}`;
+
+    // Switch statement for setting weather icon based on conditions data.
+    function setIcon (conditions) {
+        console.log(conditions)
+        switch (conditions) {
+            case ('clear sky'):
+                return 'sources/weather_icons/sunny.svg';
+            case ('broken clouds'):
+            case ('few clouds'):
+                return 'sources/weather_icons/partlycloudy.svg'
+            case ('scattered clouds'):
+            case ('overcast clouds'):
+                return 'sources/weather_icons/cloudy.svg'
+            case ('rain'):
+            case ('shower-rain'):
+                return 'sources/weather_icons/rain.svg'
+            case ('snow'):
+                return 'sources/weather_icons/snow.svg'
+            case ('mist'):
+                return 'sources/weather_icons/mist.svg'
+            default:
+                console.log('Sorry Charlie :(');
+        }   
+    };
 };
